2db09a3ab2242b23ae22a12a56362a37d6ff1b6c
LucasDias <lucasd.aragao@hotmail.com>
2022-09-08 16:26:58 -0300

    Adicionando formulario e evento de click

--- js/main.js
+++ js/main.js
@@ -38,6 +38,14 @@ for(var i = 0; i < pacientes.length; i++) {
 
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("OIIIII");
+
+});
+
+
 
 
 
