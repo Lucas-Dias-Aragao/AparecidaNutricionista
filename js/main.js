092506e62c8f52f1f06887bd6735d77df7462b54
LucasDias <lucasd.aragao@hotmail.com>
2022-09-10 02:31:44 -0300

    Adicionando pacientes

--- js/main.js
+++ js/main.js
@@ -40,15 +40,36 @@ for(var i = 0; i < pacientes.length; i++) {
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-botaoAdicionar.addEventListener("click", function(){
-    console.log("OIIIII");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault(); //previne o comportamento padrão de um evento
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd); //appendChild: coloca como filho
+    pacienteTr.appendChild(pesoTd); 
+    pacienteTr.appendChild(alturaTd); 
+    pacienteTr.appendChild(gorduraTd); 
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
 
 });
-
-
-
-
-
-
-
-console.log(imc);
